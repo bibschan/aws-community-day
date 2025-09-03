8722785551eacc77fb14fb05591be44d2d915133
Sophia Chan <sophia.gy.chan@gmail.com>
2025-09-03 09:28:44 -0700

    fix mobile speaker view

--- src/components/layout/Speakers.jsx
+++ src/components/layout/Speakers.jsx
@@ -17,13 +17,14 @@ export default function Speakers(width) {
     return (
         <section className="w-full p-8 md:p-24 relative overflow-hidden bg-white" id='speakers'>
             <div className="container px-4 md:px-6 m-auto h-full relative">
-                <div className='flex gap-4 rounded-2xl border-2 border-dashed py-10 px-6'>
-                    <div>
+                <div className='flex flex-col md:flex-row gap-4 rounded-2xl border-2 border-dashed py-10 px-6'>
+                    <div className='w-1/3 md:w-auto mx-auto max-w-40 '>
                         <Image
                             src='./speakers/speakers-icon.svg'
                             width={500}
                             height={500}
                             alt='speaker section icon'
+
                         />
                     </div>
                     <div className="flex flex-col gap-2 place-content-evenly text-center">
@@ -33,13 +34,13 @@ export default function Speakers(width) {
                         <p className="mb-2 text-left text-[#DD344C]">
                             {AWS_EVENT_CONFIG.sections.speakers.content}
                         </p>
-                        <div className="flex justify-start w-full">
+                        <div className="flex justify-start w-full ">
                             <button
                                 data-tally-open={AWS_EVENT_CONFIG.links.speakersApplicationCode}
                                 data-tally-width="800"
                                 data-tally-layout="modal"
                                 rel="AWS Day website"
-                                className="inline-block px-6 py-3 bg-[#DD344C] text-white font-medium rounded-md hover:bg-[#e68a00] transition-colors text-center"
+                                className="mx-auto md:mx-0 inline-block px-6 py-3 bg-[#DD344C] text-white font-medium rounded-md hover:bg-[#e68a00] transition-colors text-center"
                             >
                                 {AWS_EVENT_CONFIG.sections.speakers.cta.button}
                             </button>
